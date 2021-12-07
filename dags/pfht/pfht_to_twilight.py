2fcb74b296f9fb0312a4552c3eb4ce86cc6ddd76
Abdullah Malikyar <abdullah.malikyar@portlandoregon.gov>
2021-12-07 15:25:26 -0800

    [PUDL-281] finalize pfht DAG

--- dags/pfht/pfht_to_twilight.py
+++ dags/pfht/pfht_to_twilight.py
@@ -2,10 +2,12 @@ from datetime import datetime, time
 from conn_manager.conn import get_conn
 from pathlib import Path
 
-from dagster import pipeline, solid, resource, daily_schedule, repository, ModeDefinition, Field, local_file_manager
+from dagster import pipeline, solid, weekly_schedule, repository, ModeDefinition, Field, fs_io_manager, OutputDefinition, InputDefinition, List
 from dagster.config import config_schema
+from dagster.core.definitions.decorators.schedule import weekly_schedule
 # pip install dagster-azure, azure-core
 from dagster_azure.adls2 import adls2_resource, ADLS2FileManager
+from sqlalchemy.sql.expression import true
 
 
 @solid(
@@ -13,44 +15,72 @@ from dagster_azure.adls2 import adls2_resource, ADLS2FileManager
                    "remote_path": Field(str),
                    "azure_container": Field(str),
                    },
-    required_resource_keys={"adls2"}
+    required_resource_keys={"adls2"},
+    output_defs=[OutputDefinition(dagster_type=List)]
 )
 def upload_file(context):
     adls_client = context.resources.adls2.adls2_client
     local_path = Path(str(context.solid_config["local_path"]))
     path_to = context.solid_config["remote_path"]
     container = context.solid_config["azure_container"]
+    run_id = context.run_id
 
-    files: list(Path) = []
+    dirs: List(Path) = []
+    _files: List(Path) = []
 
     if local_path.is_dir():
-        files = [x for x in local_path.iterdir() if x.is_file()]
+        dirs = [x for x in local_path.iterdir() if x.is_dir()]
+        if len(dirs) > 0:
+            count = 0
+            for dir in dirs:
+                files = [x for x in dir.iterdir() if x.is_file()]
+                file_manager = ADLS2FileManager(
+                    adls2_client=adls_client, file_system=container, prefix=f"{path_to}/{dir.name}/{run_id}")
+                if len(files) > 0:
+                    count = count + len(files)
+                    for file in files:
+                        with file.open("rb") as f:
+                            file_manager.write(f, ext=str(file.suffix)[1:])
+                        _files.append(file)
+                else:
+                    context.log.info(
+                        f"No files found to upload in: {str(dir)}")
+            context.log.info(f"Number of files uploaded: {str(count)}")
+    return _files
+
+
+@solid(input_defs=[InputDefinition('files', dagster_type=List)])
+def cleanup_files(context, files):
+
+    if len(files) > 0:
+        for file in files:
+            file.unlink(missing_ok=true)
+        context.log.info(f"Number of files deleted: {str(len(files))}")
     else:
-        files = [local_path]
-
-    file_manager = ADLS2FileManager(
-        adls2_client=adls_client, file_system=container, prefix=path_to)
-
-    for file in files:
-        with file.open("rb") as f:
-            file_manager.write(f, ext=str(file.suffix)[1:])
+        context.log.info("No files found to delete.")
 
 
+# fs_io_manager is for persistent I/O that enables re-execution of tasks.
 @pipeline(
     mode_defs=[
         ModeDefinition(
-            resource_defs={'adls2': adls2_resource}
+            resource_defs={
+                'adls2': adls2_resource,
+                'io_manager': fs_io_manager
+            }
         )
     ]
 )
 def pfht_to_twilight():
-    upload_file()
+    files = upload_file()
+    cleanup_files(files=files)
 
 
-@daily_schedule(
+@weekly_schedule(
     pipeline_name="pfht_to_twilight",
-    start_date=datetime(2021, 5, 10),
-    execution_time=time(hour=12, minute=40),
+    start_date=datetime(2021, 12, 6),
+    execution_day_of_week=5,
+    execution_time=time(hour=19, minute=1),
     execution_timezone="US/Pacific",
 )
 def pfht_schedule(date):
@@ -70,8 +100,8 @@ def pfht_schedule(date):
             "upload_file": {
                 "config": {
                     "azure_container": "twilight",
-                    "local_path": "//pbotfile1/PcCommon/abdullah/test_dagster/",
-                    "remote_path": "test/pfht"
+                    "local_path": "//pbotdm1/pudl/pfht",
+                    "remote_path": "pfht"
                 }
             }
         }
