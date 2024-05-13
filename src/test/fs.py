6bc31a14caaec5d6b7dc7658036a948b74d48746
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2024-05-12 20:46:40 -0700

    feat: Add Fileshare resource
    ref: SOS-1389

--- src/test/fs.py
+++ src/test/fs.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from dagster import OpExecutionContext, fs_io_manager, job, op
 
 from ops.fs import list_dir_dynamic
+from resources.fs import FileShareResource, fileshare_resource
 
 
 @op
@@ -12,10 +13,21 @@ def show_file(context: OpExecutionContext, path: str):
     context.log.info(f"Path: {path}...")
 
 
+@op(required_resource_keys=["fileshare"])
+def upload_file(context: OpExecutionContext, path: str):
+    fs: FileShareResource = context.resources.fileshare
+    p = Path(path).resolve()
+
+    fs.upload(p, f"{p.stem}.{p.suffix}")
+
+
 @job(
     resource_defs={
         "io_manager": fs_io_manager,
-    }
+        "fileshare": fileshare_resource,
+    },
 )
 def test_list_dir_dynamic():
-    list_dir_dynamic().map(show_file)
+    paths = list_dir_dynamic()
+    paths.map(show_file)
+    paths.map(upload_file)
