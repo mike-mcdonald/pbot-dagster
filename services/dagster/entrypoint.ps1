155f954335aaf043eb6612f07399404bb2e99c10
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2024-07-01 11:42:03 -0700

    fix: entrypoint updates
    Updates webserver command and adds cryptography environment variable

--- services/dagster/entrypoint.ps1
+++ services/dagster/entrypoint.ps1
@@ -6,11 +6,12 @@ param (
 Write-Host "Switching to '$Directory'..."
 Set-Location $Directory
 Switch ($Command) {
-    "dagit" { $Command = "dagit"; $Arguments = "-l /apps/dagster -p 3030" }
+    "dagit" { $Command = "dagster-webserver"; $Arguments = "-l /apps/dagster -p 3030" }
     "daemon" { $Command = "dagster-daemon"; $Arguments = "run" }
     default { $(throw "You must specify either 'dagit' or 'dagster.'") }
 }
 $Command = Join-Path $VirtualEnv "Scripts\$Command.exe"
 $Env:PYTHONDONTWRITEBYTECODE = 1
+$Env:CRYPTOGRAPHY_OPENSSL_NO_LEGACY = 1
 Write-Host "Running '$Command'..."
 Invoke-Expression "$Command $Arguments"
