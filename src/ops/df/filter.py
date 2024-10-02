142c84a46d345380f96bb1ea78819a31d9aa2df2
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2024-10-02 15:29:42 -0700

    feat: Don't fail when dataframe to filter is empty

--- src/ops/df/filter.py
+++ src/ops/df/filter.py
@@ -7,6 +7,10 @@ from dagster import Field, OpExecutionContext, Permissive, op
 def filter_dataframe(context: OpExecutionContext, data: list[dict]) -> list[dict]:
     df = pd.DataFrame.from_records(data)
 
+    if len(df) == 0:
+        context.log.warning("No records to filter!")
+        return data
+
     for k, v in context.op_config["filters"].items():
         df = df[df[k] == v]
 
