25c0ebaf1a490f195cf7c54145ae0cf7f11a0480
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2024-10-21 21:20:46 -0700

    fix: Schedule check for files to import
    Check correct path in schedule
    Refactor repeated string values into variables
    
    ref: PCM-5

--- src/repositories/enforcement/schedules.py
+++ src/repositories/enforcement/schedules.py
@@ -8,21 +8,26 @@ from dagster import (
     SkipReason,
 )
 
-from models.connection import get_connection
 from repositories.enforcement.unregistered_vehicles import process_politess_exports
+from resources.ssh import SSHClientResource
 
 
 @schedule(cron_schedule="0 * * * *", job=process_politess_exports)
 def parking_citations_to_featureclass(context: ScheduleEvaluationContext):
     execution_stamp = context.scheduled_execution_time.strftime("%Y%m%dT%H%M%S")
+
     store_path = os.path.join(
         os.getenv("DAGSTER_DATA_BASEPATH"),
         "parking_citations_to_featureclass",
         execution_stamp,
     )
 
-    remote_path = "Vertical_Apps/Citations"
-    files = context.resources.ssh_client.list(remote_path)
+    sftp_conn_id = "sftp_parking_citations"
+    path = "/"
+
+    resource: SSHClientResource = SSHClientResource(conn_id=sftp_conn_id)
+
+    files = resource.list(path)
 
     if len(files) == 0:
         return SkipReason("No files found in import directory")
@@ -32,12 +37,12 @@ def parking_citations_to_featureclass(context: ScheduleEvaluationContext):
         run_config=RunConfig(
             resources={
                 "geodatabase": {"config": {"conn_id": "mssql_ags_pbot"}},
-                "sftp": {"config": {"conn_id": "sftp_parking_citations"}},
+                "sftp": {"config": {"conn_id": sftp_conn_id}},
             },
             ops={
                 "append_features": {
                     "config": {
-                        "feature_class": "AGS_MAINT_PBOT.PBOT_ADMIN.ParkingCitation_Test"
+                        "feature_class": "AGS_MAINT_PBOT.PBOT_ADMIN.ParkingCitation"
                     }
                 },
                 "create_dataframe": {
@@ -55,7 +60,7 @@ def parking_citations_to_featureclass(context: ScheduleEvaluationContext):
                 "download_list": {
                     "config": {"path": os.path.join(store_path, "download_list")}
                 },
-                "list": {"config": {"path": "/", "pattern": "csv"}},
+                "list": {"config": {"path": path, "pattern": "csv"}},
             },
         ),
     )
