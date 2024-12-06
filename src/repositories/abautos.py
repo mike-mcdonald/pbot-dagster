bd02a1ecbe4ff1154d2781c118361b36b8efba30
Mike McDonald <michael.mcdonald@portlandoregon.gov>
2024-12-05 17:16:58 -0800

    refactor: Combine processing, account for None
    len(None) throws an error, account for None
    Move string splitting and other processing to section where Names is first processed
    
    ref: PCM-398

--- src/repositories/abautos.py
+++ src/repositories/abautos.py
@@ -226,7 +226,20 @@ def read_reports(context: OpExecutionContext, path: str):
         lambda x: float(get_report_field(x, "report_location:location_lon"))
     )
 
-    df["Names"] = df.report_fields.map(lambda x: get_report_field(x, "contact_name"))
+    df["Names"] = (
+        df.report_fields.map(lambda x: get_report_field(x, "contact_name"))
+        .astype(str)
+        .str.rsplit()
+    )
+
+    df["FirstName"] = (
+        df["Names"]
+        .map(lambda x: x[0])
+        .map(lambda x: x if x is not None and len(x) > 0 else None)
+        .fillna("UNKNOWN")
+    )
+
+    df["LastName"] = df["Names"].map(lambda x: x[1])
 
     df["Phone"] = df.report_fields.map(lambda x: get_report_field(x, "contact_phone"))
 
@@ -320,16 +333,6 @@ def read_reports(context: OpExecutionContext, path: str):
     df["Neighborhood"] = df.apply(
         lambda x: get_neighborhood(x.Lng, x.Lat), axis="columns"
     )
-
-    df["FirstName"] = df["Names"].astype(str).str.split().str[0]
-    df["LastName"] = df["Names"].astype(str).str.split().str[1]
-
-    def get_firstName(firstName: str):
-        return firstName if len(firstName) > 0 else None
-
-    df["FirstName"] = df["FirstName"].map(get_firstName)
-
-    df["FirstName"] = df["FirstName"].fillna("UNKNOWN")
 
     df = df.fillna("")
 
